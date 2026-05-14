11a13c01ddec2506beaeace66c737a5f0f0214bd
Robert King <varitonium@gmail.com>
2026-05-14 14:59:47 +0200

    XLR-7: upe-prompt code cleaning

--- unit-test-generator/src/prompt.rs
+++ unit-test-generator/src/prompt.rs
@@ -13,35 +13,3 @@ pub fn get_prompt(lang: &str, json_path: &str) -> Result<String> {
     };
     Ok(prompt)
 }
-
-// use serde::Deserialize;
-// use std;
-// use std::fs;
-
-// #[derive(Deserialize, Debug)]
-// struct Config {
-//     rust: String,
-//     python: String,
-//     cpp: String,
-// }
-
-// fn load_config(json_path: &str) -> Result<Config, Box<dyn std::error::Error>> {
-//     let text = fs::read_to_string(json_path)?;
-//     let config: Config = serde_json::from_str(&text)?;
-//     Ok(config)
-// }
-
-// pub fn get_prompt(lang: &str) -> Result<String, Box<dyn std::error::Error>> {
-//     let config = load_config("prompt.json")?;
-
-//     let prompt: &str = match lang {
-//         "rust" => config.rust.as_str(),
-//         "python" => config.python.as_str(),
-//         "cpp" => config.cpp.as_str(),
-//         _ => {
-//             eprintln!("unsupported language: {}", lang);
-//             std::process::exit(1);
-//         }
-//     };
-//     Ok(prompt.to_string())
-// }
