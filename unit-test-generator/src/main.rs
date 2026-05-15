bd675f36ad7b1e7c3cb3d9b82af4bc0488e85c91
Robert King <varitonium@gmail.com>
2026-05-15 14:33:11 +0200

    XLR-10: upe-adding of llm requester impl     *adding of Args POD

--- unit-test-generator/src/main.rs
+++ unit-test-generator/src/main.rs
@@ -1,62 +1,35 @@
+mod args;
 mod config;
 mod config_mgr;
 mod iofilehdr;
+mod llm_requester;
 mod prompt;
 
-use anyhow::{Context, Result};
-use async_openai::{Client, config::OpenAIConfig, types::responses::CreateResponseArgs};
-use clap::Parser;
+use anyhow::Result;
 use std;
 use std::env;
-use std::fs;
 
-use iofilehdr::{read, write};
+use args::Args;
+use iofilehdr::write;
+use llm_requester::LLMRequester;
 use prompt::get_prompt;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about,long_about = None)]
-struct Args {
-    /// Input file
-    input: String,
-
-    //main stream language to choose
-    #[arg(short, long)]
-    lang: String,
-
-    /// Number of times
-    #[arg(short, long, default_value_t = 1)]
-    count: usize,
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let key = env::var("OPEN_AI_KEY")?;
-    let args = Args::parse();
+    let args = Args::parse_args();
 
     if args.count == 0 {
         eprintln!("missing argument");
         std::process::exit(1);
     }
 
-    let config = OpenAIConfig::new().with_api_key(key);
-    let client = Client::with_config(config);
+    let prompt = get_prompt(&args)?;
 
-    let code = read(&args.input)?;
-    let prompt_template = get_prompt(&args.lang, "prompts.json")?;
-    let prompt = format!("{} {}", prompt_template, code);
+    let mut llm_requester = LLMRequester::new(key, "gpt-4.1".to_string(), 512);
+    llm_requester.init()?;
+    let response = llm_requester.request(&prompt).await?;
 
-    let request = CreateResponseArgs::default()
-        .model("gpt-4.1")
-        .input(&prompt)
-        .max_output_tokens(512u32)
-        .build()?;
-
-    // Call API
-    let response = client
-        .responses() // Get the API "group" (responses, images, etc.) from the client
-        .create(request) // Make the API call in that "group"
-        .await?;
-
-    write("output.txt", &response.output_text().unwrap())?;
+    write(&args.output, &response.output_text().unwrap())?;
     Ok(())
 }
--- unit-test-generator/src/prompt.rs
+++ unit-test-generator/src/prompt.rs
@@ -1,15 +1,22 @@
+use crate::args::Args;
 use crate::config::Config;
 use crate::config_mgr::load_config;
+use crate::iofilehdr::read;
 use anyhow::Result;
 
-pub fn get_prompt(lang: &str, json_path: &str) -> Result<String> {
-    let config = load_config::<Config>(json_path)?;
+pub fn get_prompt(args: &Args) -> Result<String> {
+    let code = read(&args.input)?;
 
-    let prompt = match lang {
+    let config = load_config::<Config>(&args.conf)?;
+
+    let prompt_template = match args.lang.as_str() {
         "rust" => config.rust,
         "python" => config.python,
         "cpp" => config.cpp,
-        _ => anyhow::bail!("unsupported language: {}", lang),
+        _ => anyhow::bail!("unsupported language: {}", args.lang),
     };
+
+    let prompt = format!("{} {}", prompt_template, code);
+
     Ok(prompt)
 }
